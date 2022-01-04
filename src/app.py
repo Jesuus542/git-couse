3c8091edd0cf5b6765a3e3be22487dbff1c17391
Jesuus542 <94186004+Jesuus542@users.noreply.github.com>
2022-01-04 12:34:25 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,4 +5,8 @@ def index():
 return "Bye Jesus!"
 @app.route('/sum/<int:a>/<int:b>')
 def sum(a: int, b: int):
-return a + b
+  nums_sum = a + b
+return f"la suma es : {str(nums_sum)}"
+@app.route('/multiply/<int:a</<int:b>')
+def multiply = floar (a * b)
+retur f"el resultado de la multipli es: {str(result)}"
